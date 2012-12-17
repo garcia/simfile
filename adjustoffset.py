0015d6d08b80931ae1eeaa7698ccf755a22f631d
Ash Garcia <github@garcia.sh>
2012-12-16 22:07:49 -0700

    Reworked adjustoffset to use new Simfile methods

--- adjustoffset.py
+++ adjustoffset.py
@@ -13,16 +13,14 @@ def adjustoffset(simfile):
     log = logging.getLogger('synctools')
     cfg = synctools.get_config()
     synctools.backup(simfile)
-    with codecs.open(simfile.filename, 'w', 'utf-8') as output:
-        for param in simfile.params:
-            if param[0].upper() == 'OFFSET':
-                # Process offset
-                offset = Decimal(param[1])
-                offset2 = offset + Decimal(cfg['adjustoffset']['amount'])
-                log.info('%s -> %s' % (str(offset), str(offset2)))
-                # Rewrite parameter
-                param[1] = str(offset2)
-            output.write(unicode(param) + '\n')
+    offset_param = simfile.get('OFFSET')
+    # Process offset
+    offset = Decimal(offset_param[1])
+    offset2 = offset + Decimal(cfg['adjustoffset']['amount'])
+    log.info('%s -> %s' % (str(offset), str(offset2)))
+    # Rewrite parameter
+    offset_param[1] = str(offset2)
+    simfile.save()
 
 
 if __name__ == '__main__':
