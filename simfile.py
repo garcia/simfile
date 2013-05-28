ccb5854081fab5fb318ee12c58df809885316183
Ash Garcia <github@garcia.sh>
2013-05-28 12:47:21 -0700

    v0.8.0

--- simfile.py
+++ simfile.py
@@ -7,7 +7,7 @@ import os
 __author__ = 'Grant Garcia'
 __copyright__ = 'Copyright 2013, Grant Garcia'
 __license__ = 'MIT'
-__version__ = '0.7.2'
+__version__ = '0.8.0'
 
 __all__ = ['decimal_to_192nd', 'decimal_from_192nd', 'Param', 'Notes', 'Chart',
            'Timing', 'Simfile']
