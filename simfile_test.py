11c143f0d4b764640c8cad9bdaedab82d990e578
Ash Garcia <github@garcia.sh>
2013-07-05 18:28:04 -0700

    Updating test cases for upcoming overhaul

--- simfile_test.py
+++ simfile_test.py
@@ -1,106 +1,117 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import with_statement, unicode_literals
+import codecs
 import decimal
 import os
 import unittest
 
-from simfile import *
+from simfile import Simfile, Timing
+from simfile.msd import MSDParser
+
+def get_parser(filename):
+    return MSDParser(codecs.open(os.path.join('testdata', filename), 'r',
+        encoding='utf-8'))
+
+def get_simfile(filename):
+    return Simfile(os.path.join('testdata', filename))
+
+class TestMSD(unittest.TestCase):
+    
+    def test_empty(self):
+        parser = get_parser('empty.sm')
+        # Generator should exist
+        self.assertTrue(parser)
+        # Generator should yield absolutely nothing
+        for param in parser:
+            self.fail()
+    
+    def test_comments(self):
+        parser = get_parser('comments.sm')
+        self.assertEqual(parser.next(), ['TITLE', 'Comments'])
+        self.assertEqual(parser.next(), ['SUBTITLE', 'Split into two lines'])
+        self.assertEqual(parser.next(), ['ARTIST', 'Grant/Garcia'])
+        self.assertRaises(StopIteration, parser.next)
+
+    def test_duplicates(self):
+        parser = get_parser('duplicates.sm')
+        self.assertEqual(parser.next(), ['TITLE', 'First duplicate field'])
+        self.assertEqual(parser.next(), ['TITLE', 'Second duplicate field'])
+        self.assertEqual(parser.next(), ['SUBTITLE', 'CASE INSENSITIVITY'])
+        self.assertEqual(parser.next(), ['Subtitle', 'case insensitivity'])
+        self.assertRaises(StopIteration, parser.next)
+
+    def test_multivalue(self):
+        parser = get_parser('multivalue.sm')
+        self.assertEqual(parser.next(), ['TITLE', 'One value'])
+        self.assertEqual(parser.next(), ['SUBTITLE', 'First value', 'second value'])
+        self.assertEqual(parser.next(), ['ARTIST', 'One', 'two', 'three'])
+        self.assertRaises(StopIteration, parser.next)
+
+    def test_no_semicolon(self):
+        parser = get_parser('no semicolon.sm')
+        self.assertEqual(parser.next(), ['TITLE', 'No semicolon'])
+        self.assertEqual(parser.next(), ['SUBTITLE', 'EOF'])
+        self.assertRaises(StopIteration, parser.next)
+
+    def test_unicode(self):
+        parser = get_parser('unicode.sm')
+        self.assertEqual(parser.next(), ['TITLE', '実例'])
+        self.assertEqual(parser.next(), ['ARTIST', '楽士'])
+        self.assertRaises(StopIteration, parser.next)
 
 class TestSimfile(unittest.TestCase):
     
-    cache = {}
-    
-    def get_simfile(self, filename, clone=False):
-        if filename not in self.cache:
-            self.cache[filename] = Simfile(os.path.join('testdata', filename))
-        if clone:
-            return Simfile(string=unicode(self.cache[filename]))
-        else:
-            return self.cache[filename]
-    
-    def test_empty(self):
-        sm = self.get_simfile('empty.sm')
-        self.assertTrue(sm)
-        self.assertFalse(sm.params)
-        self.assertRaises(KeyError, sm.get, 'TITLE')
-        # Nowhere else to put these at the moment
+    def test_init(self):
+        # Can't pass both filename and string
         self.assertRaises(TypeError, Simfile, filename='.', string='.')
+        sm1 = Simfile('testdata/Tribal Style.sm')
+        # Named 'filename' argument can be positionally inferred
+        self.assertEqual(sm1, Simfile(filename='testdata/Tribal Style.sm'))
+        # Reading file from string should return identical results
+        # (except for the filename and dirname properties, but those aren't
+        # considered when comparing simfiles)
+        with codecs.open('testdata/Tribal Style.sm', 'r', 'utf-8') as sm2:
+            self.assertEqual(sm1, Simfile(string=sm2.read()))
     
-    def test_get(self):
-        sm = self.get_simfile('Tribal Style.sm')
-        self.assertEqual(sm.get('TITLE'), Param(('TITLE', 'Tribal Style')))
-        self.assertRaises(ValueError, sm.get, 'NOTES')
-    
-    def test_get_string(self):
-        sm = self.get_simfile('Tribal Style.sm')
-        self.assertEqual(sm.get_string('TITLE'), 'Tribal Style')
-        self.assertRaises(ValueError, sm.get_string, 'NOTES')
-    
-    def test_comments(self):
-        sm = self.get_simfile('comments.sm')
-        self.assertEqual(sm.get('TITLE'), Param(('TITLE', 'Comments')))
-        self.assertEqual(sm.get('SUBTITLE'), Param(('SUBTITLE', 'Split into two lines')))
-        self.assertEqual(sm.get('ARTIST'), Param(('ARTIST', 'Grant/Garcia')))
-        self.assertEqual(sm.get_string('TITLE'), 'Comments')
-
-    def test_duplicates(self):
-        sm = self.get_simfile('duplicates.sm')
-        self.assertEqual(sm.get('TITLE'), Param(('TITLE', 'First duplicate field')))
-        self.assertEqual(sm.get('TITLE', 1), Param(('TITLE', 'Second duplicate field')))
-        self.assertEqual(sm.get('SUBTITLE', 0), Param(('SUBTITLE', 'CASE INSENSITIVITY')))
-        self.assertEqual(sm.get('SUBTITLE', 1), Param(('Subtitle', 'case insensitivity')))
-
-    def test_multivalue(self):
-        sm = self.get_simfile('multivalue.sm')
-        self.assertEqual(sm.get('TITLE'), Param(('TITLE', 'One value')))
-        self.assertEqual(sm.get('SUBTITLE'), Param(('SUBTITLE', 'First value', 'second value')))
-        self.assertEqual(sm.get('ARTIST'), Param(('ARTIST', 'One', 'two', 'three')))
-        self.assertEqual(sm.get_string('SUBTITLE'), 'First value:second value')
-        self.assertEqual(sm.get_string('ARTIST'), 'One:two:three')
-
-    def test_no_semicolon(self):
-        sm = self.get_simfile('no semicolon.sm')
-        self.assertEqual(sm.get('TITLE'), Param(('TITLE', 'No semicolon')))
-        self.assertEqual(sm.get('SUBTITLE'), Param(('SUBTITLE', 'EOF')))
-
-    def test_unicode(self):
-        sm1 = self.get_simfile('unicode.sm')
-        sm2 = Simfile(string=unicode(sm1))
-        self.assertEqual(sm1.get('TITLE'), Param(('TITLE', u'実例')));
-        self.assertEqual(sm1.get('ARTIST'), Param(('ARTIST', u'楽士')));
-        self.assertEqual(sm1.get('TITLE'), sm2.get('TITLE'));
-
-
     def test_types(self):
-        sm = self.get_simfile('Tribal Style.sm')
-        self.assertIsInstance(sm.get('TITLE'), Param)
-        bpms_param = sm.get('BPMS')
-        self.assertIsInstance(bpms_param, Param)
-        self.assertIsInstance(bpms_param[1], Timing)
-        stops_param = sm.get('STOPS')
-        self.assertIsInstance(stops_param[1], Timing)
-        chart = sm.get_chart(index=0)
-        self.assertIsInstance(chart, Chart)
-        self.assertIsInstance(chart.notes, Notes)
+        sm = get_simfile('Tribal Style.sm')
+        self.assertIsInstance(sm['TITLE'], basestring)
+        self.assertIsInstance(sm['BPMS'], Timing)
+        self.assertIsInstance(sm['STOPS'], Timing)
+        #chart = sm.get_chart(index=0)
+        #self.assertIsInstance(chart, Chart)
+        #self.assertIsInstance(chart.notes, Notes)
+    
+    def test_getitem(self):
+        sm = get_simfile('Tribal Style.sm')
+        # Basic parameter retrieval
+        self.assertIn('TITLE', sm)
+        self.assertEqual(sm['TITLE'], 'Tribal Style')
+        # Charts should reside in sm.charts
+        self.assertNotIn('NOTES', sm)
+        # Despite being 'weird', BPMS and STOPS are still items of sm
+        self.assertIn('BPMS', sm)
+        self.assertIn('STOPS', sm)
 
     def test_bpms(self):
-        sm = self.get_simfile('Robotix.sm')
-        bpms = sm.get('BPMS')[1]
+        sm = get_simfile('Robotix.sm')
+        bpms = sm['BPMS']
         self.assertEqual(bpms[0][0], 0)
         self.assertEqual(bpms[0][1], 150)
         self.assertEqual(bpms[1][0], 144)
         self.assertEqual(bpms[1][1], decimal.Decimal('170.001'))
 
     def test_stops(self):
-        sm = self.get_simfile('Robotix.sm')
-        stops = sm.get('STOPS')[1]
+        sm = get_simfile('Robotix.sm')
+        stops = sm['STOPS']
         self.assertEqual(stops[0][0], 313)
         self.assertEqual(stops[0][1], decimal.Decimal('0.400'))
         self.assertEqual(stops[1][0], 344)
         self.assertEqual(stops[1][1], decimal.Decimal('0.400'))
 
-    def test_get_chart(self):
-        sm = self.get_simfile('Tribal Style.sm')
+    """def test_get_chart(self):
+        sm = get_simfile('Tribal Style.sm')
         chart_sx = sm.get_chart(stepstype='dance-single', difficulty='Challenge')
         self.assertEqual(chart_sx.meter, 10)
         chart_sh = sm.get_chart(stepstype='dance-single', difficulty='Hard')
@@ -119,7 +130,7 @@ class TestSimfile(unittest.TestCase):
         self.assertRaises(KeyError, sm.get_chart, meter=100)
 
     def test_param_unicode(self):
-        sm = self.get_simfile('Tribal Style.sm')
+        sm = get_simfile('Tribal Style.sm')
         self.assertEqual(unicode(sm.get('TITLE')), '#TITLE:Tribal Style;')
         self.assertEqual(unicode(sm.get('Artist')), '#ARTIST:KaW;')
         bpms_param = sm.get('BPMS')
@@ -130,7 +141,7 @@ class TestSimfile(unittest.TestCase):
         self.assertEqual(unicode(stops_param[1]), '')
 
     def test_chart_unicode(self):
-        sm = self.get_simfile('Tribal Style.sm')
+        sm = get_simfile('Tribal Style.sm')
         chart_sn = sm.get_chart(meter=1)
         expected_str = (
             "#NOTES:\n"
@@ -146,7 +157,7 @@ class TestSimfile(unittest.TestCase):
         # Comprehensive test that ensures unicode(simfile) returns a perfect
         # representation of the original simfile. This also serves as a "test"
         # of Simfile.save(), which essentially writes unicode(self) to a file.
-        sm1 = self.get_simfile('Tribal Style.sm')
+        sm1 = get_simfile('Tribal Style.sm')
         sm2 = Simfile(string=unicode(sm1))
         self.assertEqual(sm1, sm2)
     
@@ -155,11 +166,11 @@ class TestSimfile(unittest.TestCase):
     # this way.)
     
     def test_pop(self):
-        sm1 = self.get_simfile('Tribal Style.sm', clone=True)
+        sm1 = get_simfile('Tribal Style.sm', clone=True)
         title = sm1.pop('TITLE')
         self.assertEqual(title, Param(('TITLE', 'Tribal Style')))
         self.assertRaises(KeyError, sm1.pop, 'TITLE')
-        sm2 = self.get_simfile('duplicates.sm', clone=True)
+        sm2 = get_simfile('duplicates.sm', clone=True)
         self.assertEqual(sm2.pop('TITLE'), Param(('TITLE', 'First duplicate field')))
         self.assertEqual(sm2.pop('TITLE'), Param(('TITLE', 'Second duplicate field')))
         self.assertRaises(KeyError, sm2.pop, 'TITLE')
@@ -169,7 +180,7 @@ class TestSimfile(unittest.TestCase):
         self.assertRaises(KeyError, sm2.pop, 'SUBTITLE')
     
     def test_pop_chart(self):
-        sm = self.get_simfile('Tribal Style.sm', clone=True)
+        sm = get_simfile('Tribal Style.sm', clone=True)
         chart1 = sm.get_chart(index=0)
         chart2 = sm.pop_chart(index=0)
         self.assertEqual(chart1, chart2)
@@ -194,7 +205,7 @@ class TestSimfile(unittest.TestCase):
         self.assertEqual(len([param for param in sm]), 2)
         sm.set('PARAM3')
         self.assertEqual(sm.get('PARAM3'), Param(('PARAM3',)))
-        self.assertEqual(len([param for param in sm]), 3)
+        self.assertEqual(len([param for param in sm]), 3)"""
 
 
 if __name__ == '__main__':
