e2d7d0c72967d8cc57e502c5dedecb0e8fef8e43
Ash Garcia <github@garcia.sh>
2014-04-20 16:20:59 -0700

    Copyright year bump

--- simfile/__init__.py
+++ simfile/__init__.py
@@ -1,6 +1,6 @@
 from simfile import *
 
 __author__ = 'Grant Garcia'
-__copyright__ = 'Copyright 2013, Grant Garcia'
+__copyright__ = 'Copyright 2014, Grant Garcia'
 __license__ = 'MIT'
 __version__ = '1.0.0'
