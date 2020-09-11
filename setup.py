c179f2fc694c832de6cb5bdd31c9e64d5f0e074c
Ash Garcia <github@garcia.sh>
2020-09-10 23:49:08 -0700

    Split msdparser into its own package

--- setup.py
+++ setup.py
@@ -5,13 +5,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-      name='simfile',
-      version='2.0.0-alpha',
-      author='Ash Garcia',
-      author_email='python-simfile@garcia.sh'
-      description='A simfile parser for Python',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      url='https://github.com/garcia/simfile',
-      packages=['simfile'],
+    name='simfile',
+    version='2.0.0-alpha',
+    author='Ash Garcia',
+    author_email='python-simfile@garcia.sh'
+    description='A simfile parser for Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/garcia/simfile',
+    packages=['simfile'],
+    install_requires=[
+        'msdparser',
+    ],
 )
