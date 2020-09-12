c5af4308658b46cc9e8d5e5970850ec55442838f
Ash Garcia <github@garcia.sh>
2020-09-12 00:02:08 -0700

    Fix SSCSimfile._parse

--- simfile/ssc.py
+++ simfile/ssc.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict
+from typing import Optional
 
 from .base import BaseChart, BaseCharts, BaseSimfile
-from .common import *
 from msdparser import MSDParser
 
 
@@ -35,7 +35,7 @@ class SSCChart(BaseChart, OrderedDict):
 
     def serialize(self, file):
         file.write('#NOTEDATA:;\n')
-        for (key, value) in self:
+        for (key, value) in self.items():
             file.write(f'#{key}:{value};\n')
         file.write('\n')
 
@@ -54,11 +54,18 @@ class SSCSimfile(BaseSimfile):
     def _parse(self):
         with MSDParser(file=self.file, string=self.string) as parser:
             self._charts = SSCCharts()
+            partial_chart: Optional[SSCChart] = None
             for (key, value) in parser:
-                if key == 'NOTES':
-                    self._charts.append(SSCChart(string=value))
+                if key == 'NOTEDATA':
+                    if partial_chart is not None:
+                        self._charts.append(partial_chart)
+                    partial_chart = SSCChart()
+                elif partial_chart is not None:
+                    partial_chart[key] = value
                 else:
                     self[key] = value
+            if partial_chart is not None:
+                self._charts.append(partial_chart)
     
     @property
     def charts(self):
