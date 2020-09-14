9f569cf27f1ee9486716a618711cd1e4fe899560
Ash Garcia <github@garcia.sh>
2020-09-13 20:54:35 -0700

    Reorganize setup.py

--- setup.py
+++ setup.py
@@ -10,18 +10,13 @@ with open("README.md", "r") as fh:
 setup(
     name='simfile',
     version=version,
-    author='Ash Garcia',
-    author_email='python-simfile@garcia.sh',
     description='Modern simfile library for Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
+    author='Ash Garcia',
+    author_email='python-simfile@garcia.sh',
     url='https://github.com/garcia/simfile',
     packages=['simfile'],
-    command_options={
-        'build_sphinx': {
-            'version': ('setup.py', version),
-        },
-    },
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3',
@@ -29,11 +24,17 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Typing :: Typed',
     ],
+    zip_safe=False,
     install_requires=[
         'msdparser>=0.1.3',
     ],
+    python_requires='>=3.6',
     tests_require=[
         'pyfakefs'
     ],
-    zip_safe=False,
+    command_options={
+        'build_sphinx': {
+            'version': ('setup.py', version),
+        },
+    },
 )
