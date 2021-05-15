ad035b74ed04756311b24d0cc8d92f710121b735
ash garcia <github@garcia.sh>
2021-05-15 12:40:01 -0700

    convert: tweak enums, add documentation

--- simfile/convert.py
+++ simfile/convert.py
@@ -8,7 +8,7 @@ from .ssc import SSCChart, SSCSimfile
 
 
 __all__ = [
-    'InvalidProperty', 'InvalidPropertyBehavior', 'InvalidPropertyException',
+    'KnownProperty', 'InvalidPropertyBehavior', 'InvalidPropertyException',
     'sm_to_ssc', 'ssc_to_sm',
 ]
 
@@ -23,35 +23,42 @@ DEFAULT_PROPERTIES: DefaultDict[str, str] = defaultdict(lambda: '', {
 })
 
 
-class InvalidProperty(Enum):
-    MUST_IGNORE = 1
+class KnownProperty(Enum):
+    """
+    Types of known properties.
+
+    These mirror the lists of known properties documented in
+    :class:`simfile.base.BaseSimfile` and
+    :class:`simfile.ssc.SSCSimfile`.
+    """
+    SSC_VERSION = 1
     METADATA = 2
     FILE_PATH = 3
     GAMEPLAY_EVENT = 4
     TIMING_DATA = 5
 
 
-TYPE_TO_INVALID_PROPERTIES: Dict[Type, Dict[InvalidProperty, List[str]]] = {
+INVALID_PROPERTIES: Dict[Type, Dict[KnownProperty, List[str]]] = {
     SMSimfile: {
-        InvalidProperty.MUST_IGNORE: ['VERSION'],
-        InvalidProperty.METADATA: ['ORIGIN', 'TIMESIGNATURES', 'LABELS'],
-        InvalidProperty.FILE_PATH: [
+        KnownProperty.SSC_VERSION: ['VERSION'],
+        KnownProperty.METADATA: ['ORIGIN', 'TIMESIGNATURES', 'LABELS'],
+        KnownProperty.FILE_PATH: [
             'PREVIEWVID', 'JACKET', 'CDIMAGE', 'DISCIMAGE',
         ],
-        InvalidProperty.GAMEPLAY_EVENT: [
+        KnownProperty.GAMEPLAY_EVENT: [
             'TICKCOUNTS', 'COMBOS', 'SPEEDS', 'SCROLLS', 'FAKES',
         ],
-        InvalidProperty.TIMING_DATA: ['DELAYS', 'WARPS'],
+        KnownProperty.TIMING_DATA: ['DELAYS', 'WARPS'],
     },
     SMChart: {
-        InvalidProperty.METADATA: [
+        KnownProperty.METADATA: [
             'CHARTNAME', 'CHARTSTYLE', 'CREDIT', 'DISPLAYBPM',
             'TIMESIGNATURES', 'LABELS',
         ],
-        InvalidProperty.GAMEPLAY_EVENT: [
+        KnownProperty.GAMEPLAY_EVENT: [
             'TICKCOUNTS', 'COMBOS', 'SPEEDS', 'SCROLLS', 'FAKES',
         ],
-        InvalidProperty.TIMING_DATA: [
+        KnownProperty.TIMING_DATA: [
             'OFFSET', 'BPMS', 'STOPS', 'DELAYS', 'WARPS',
         ],
     },
@@ -59,25 +66,45 @@ TYPE_TO_INVALID_PROPERTIES: Dict[Type, Dict[InvalidProperty, List[str]]] = {
 
 
 class InvalidPropertyBehavior(Enum):
+    """
+    How to handle an invalid property during conversion.
+
+    If a known property can't be converted to the destination type...
+
+    * `COPY_ANYWAY`: copy the property regardless
+    * `IGNORE`: do not copy the property
+    * `ERROR_UNLESS_DEFAULT`: raise :class:`InvalidPropertyException`
+      unless its value is the default value
+    * `ERROR`: raise :class:`InvalidPropertyException` regardless of
+      the property's value
+
+    The "default value" for most properties is an empty string. If the
+    destination type's :code:`.blank` output has a non-empty value for
+    the property, that value is considered the default instead (except
+    for `BPMS`).
+    """
     COPY_ANYWAY = 1
     IGNORE = 2
     ERROR_UNLESS_DEFAULT = 3
     ERROR = 4
 
 
-InvalidPropertyBehaviorMapping = Dict[InvalidProperty, InvalidPropertyBehavior]
+InvalidPropertyBehaviorMapping = Dict[KnownProperty, InvalidPropertyBehavior]
 
 
 INVALID_PROPERTY_BEHAVIORS: InvalidPropertyBehaviorMapping = {
-    InvalidProperty.MUST_IGNORE: InvalidPropertyBehavior.IGNORE,
-    InvalidProperty.METADATA: InvalidPropertyBehavior.IGNORE,
-    InvalidProperty.FILE_PATH: InvalidPropertyBehavior.IGNORE,
-    InvalidProperty.GAMEPLAY_EVENT: InvalidPropertyBehavior.ERROR_UNLESS_DEFAULT,
-    InvalidProperty.TIMING_DATA: InvalidPropertyBehavior.ERROR,
+    KnownProperty.SSC_VERSION: InvalidPropertyBehavior.IGNORE,
+    KnownProperty.METADATA: InvalidPropertyBehavior.IGNORE,
+    KnownProperty.FILE_PATH: InvalidPropertyBehavior.IGNORE,
+    KnownProperty.GAMEPLAY_EVENT: InvalidPropertyBehavior.ERROR_UNLESS_DEFAULT,
+    KnownProperty.TIMING_DATA: InvalidPropertyBehavior.ERROR,
 }
 
 
 class InvalidPropertyException(Exception):
+    """
+    Raised by conversion functions if a property cannot be converted.
+    """
     # TODO: human-friendly string
     pass
 
@@ -92,7 +119,7 @@ _CONVERT_CHART = TypeVar('_CONVERT_CHART', SMChart, SSCChart)
 def _should_copy_property(
     property: str,
     value: str,
-    invalid_properties: Dict[InvalidProperty, List[str]],
+    invalid_properties: Dict[KnownProperty, List[str]],
     invalid_property_behaviors: InvalidPropertyBehaviorMapping
 ) -> bool:
     for invalid_property, properties in invalid_properties.items():
@@ -119,7 +146,7 @@ def _copy_properties(
     output_type: Type[_CONVERT_TYPE],
     invalid_property_behaviors: InvalidPropertyBehaviorMapping,
 ) -> None:
-    invalid_properties = TYPE_TO_INVALID_PROPERTIES.get(output_type, {})
+    invalid_properties = INVALID_PROPERTIES.get(output_type, {})
     for property, value in source.items():
         if _should_copy_property(
             property,
@@ -174,15 +201,23 @@ def sm_to_ssc(
     *,
     simfile_template: Optional[SSCSimfile] = None,
     chart_template: Optional[SSCChart] = None,
-    invalid_property_behaviors: InvalidPropertyBehaviorMapping = {},
 ) -> SSCSimfile:
+    """
+    Convert an SM simfile to an equivalent SSC simfile.
+
+    `simfile_template` and `chart_template` can optionally be provided
+    to define the initial simfile and chart prior to copying properties
+    from the source object. If they are not provided,
+    :meth:`simfile.sm.SMSimfile.blank` and
+    :meth:`simfile.sm.SMChart.blank` will supply the template objects.
+    """
     return _convert(
         simfile=sm_simfile,
         output_simfile_type=SSCSimfile,
         output_chart_type=SSCChart,
         simfile_template=simfile_template,
         chart_template=chart_template,
-        invalid_property_behaviors=invalid_property_behaviors,
+        invalid_property_behaviors={},
     )
 
 
@@ -192,7 +227,30 @@ def ssc_to_sm(
     simfile_template: Optional[SMSimfile] = None,
     chart_template: Optional[SMChart] = None,
     invalid_property_behaviors: InvalidPropertyBehaviorMapping = {},
-) -> SMSimfile:
+) -> SMSimfile: 
+    """
+    Convert an SSC simfile to an equivalent SM simfile.
+
+    `simfile_template` and `chart_template` can optionally be provided
+    to define the initial simfile and chart prior to copying properties
+    from the source object. If they are not provided,
+    :meth:`simfile.ssc.SSCSimfile.blank` and
+    :meth:`simfile.ssc.SSCChart.blank` will supply the template objects.
+
+    Not all SSC properties are valid for SM simfiles, and some of these
+    properties may be crucial for the simfile to behave as intended
+    during gameplay. By default, invalid properties are ignored unless:
+
+    * The property is a gameplay event and has a non-default value, or
+    * The property contains timing data.
+
+    These behaviors can be overridden by supplying the
+    `invalid_property_behaviors` parameter, which maps
+    :class:`KnownProperty` to :class:`InvalidPropertyBehavior` values.
+    This mapping need not cover every :class:`KnownProperty`; any
+    missing values will fall back to the default mapping described
+    above.
+    """
     return _convert(
         simfile=ssc_simfile,
         output_simfile_type=SMSimfile,
