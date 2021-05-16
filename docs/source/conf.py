008793764b56c4b8614154a3a3466f64fe573236
ash garcia <github@garcia.sh>
2021-05-15 20:18:38 -0700

    improve CSS for multi-arg callables

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,6 +67,8 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_css_files = ['css/custom.css']
+
 autoapi_type = 'python'
 autoapi_dirs = ['../../simfile']
 autoapi_ignore = ['*tests*', '*_private*']
