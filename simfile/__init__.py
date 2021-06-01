29d34a9a0df9dc16aa6f0610396381bbd3330e7f
Ash Garcia <github@garcia.sh>
2021-06-01 16:45:50 -0700

    try multiple encodings when opening from filesystem

--- simfile/__init__.py
+++ simfile/__init__.py
@@ -1,7 +1,7 @@
 import builtins
 from contextlib import contextmanager
 from io import StringIO
-from typing import Iterator, TextIO, Union
+from typing import Iterator, List, Optional, TextIO, Tuple, Union
 
 from msdparser import MSDParser
 
@@ -12,13 +12,31 @@ from ._private.tee_file import tee_file
 
 
 __version__ = '2.0.0-beta.3'
-__all__ = ['load', 'loads', 'open', 'CancelMutation', 'mutate']
+__all__ = [
+    'load', 'loads', 'open', 'open_with_detected_encoding', 'CancelMutation',
+    'mutate',
+]
 
 
-def _open_args(kwargs):
-    open_args = {'encoding': 'utf-8'}
-    open_args.update(kwargs)
-    return open_args
+ENCODINGS = ['utf-8', 'cp1252', 'cp932', 'cp949']
+
+
+def _detect_ssc(peek_file: Union[TextIO, Iterator[str]]) -> bool:
+    if isinstance(peek_file, TextIO) and type(peek_file.name) is str:
+        _, _, suffix = peek_file.name.rpartition('.')
+        if suffix == '.ssc':
+            return True
+        elif suffix == '.sm':
+            return False
+
+    # Peek at the first property next
+    parser = MSDParser(file=peek_file)
+    first_key = ''
+    for first_key, _ in parser:
+        break
+
+    # Check for .SSC version header next
+    return first_key.upper() == 'VERSION'
 
 
 def load(file: Union[TextIO, Iterator[str]]) -> Simfile:
@@ -31,27 +49,9 @@ def load(file: Union[TextIO, Iterator[str]]) -> Simfile:
     the file is treated as an SSC simfile; otherwise, it's treated as
     an SM simfile.
     """
-    
-    # Check for filename hint first
-    if isinstance(file, TextIO) and type(file.name) is str:
-        _, _, suffix = file.name.rpartition('.')
-        if suffix == '.ssc':
-            return SSCSimfile(file=file)
-        elif suffix == '.sm':
-            return SMSimfile(file=file)    
-
-    # Peek at the first property next
-    peek, file = tee_file(file)
-    parser = MSDParser(file=peek)
-    first_key = ''
-    for first_key, _ in parser:
-        break
-
-    # Check for .SSC version header next
-    if first_key.upper() == 'VERSION':
-        return SSCSimfile(file=file)
-    else:
-        return SMSimfile(file=file)
+    peek_file, file = tee_file(file)
+    is_ssc = _detect_ssc(peek_file)
+    return SSCSimfile(file=file) if is_ssc else SMSimfile(file=file)
 
 
 def loads(string: str = None) -> Simfile:
@@ -66,10 +66,59 @@ def open(filename: str, **kwargs) -> Simfile:
     Load a simfile by filename using the correct implementation.
 
     Keyword arguments are passed to the builtin `open` function.
-    Encoding defaults to UTF-8.
+    If no encoding is specified, this function will defer to
+    :func:`open_with_detected_encoding`.
     """
-    with builtins.open(filename, 'r', **_open_args(kwargs)) as file:
-        return load(file)
+    try_encodings = ENCODINGS
+    if 'encoding' in kwargs:
+        try_encodings = [kwargs.pop('encoding')]
+    
+    return open_with_detected_encoding(
+        filename,
+        try_encodings,
+        **kwargs
+    )[0]
+
+
+def open_with_detected_encoding(
+    filename: str,
+    try_encodings: List[str] = ENCODINGS,
+    **kwargs
+) -> Tuple[Simfile, str]:
+    """
+    Load a simfile by filename using the correct implementation & encoding.
+
+    Tries decoding the simfile as UTF-8, CP1252 (English), CP932
+    (Japanese), and CP949 (Korean), mirroring the encodings supported
+    by StepMania. This list can be overridden by supplying `try_encodings`.
+    """
+    if 'encoding' in kwargs:
+        raise TypeError(
+            "unexpected encoding argument - use try_encodings instead"
+        )
+
+    exception: Optional[UnicodeDecodeError] = None
+    
+    for encoding in try_encodings:
+        try:
+            with builtins.open(
+                filename,
+                'r',
+                encoding=encoding,
+                **kwargs
+            ) as file:
+                return (load(file), encoding)
+        except UnicodeDecodeError as e:
+            # Keep track of each encoding's exception
+            if exception:
+                e.__cause__ = exception
+                exception = e
+            else:
+                exception = e
+            continue
+    
+    # If all encodings failed, raise the exception chain
+    raise exception or UnicodeDecodeError
 
 
 class CancelMutation(BaseException):
@@ -79,7 +128,11 @@ class CancelMutation(BaseException):
 
 
 @contextmanager
-def mutate(filename: str, **kwargs) -> Iterator[Simfile]:
+def mutate(
+    filename: str,
+    try_encodings: List[str] = ENCODINGS,
+    **kwargs
+) -> Iterator[Simfile]:
     """
     Context manager that loads & saves a simfile by filename.
 
@@ -88,9 +141,15 @@ def mutate(filename: str, **kwargs) -> Iterator[Simfile]:
     to re-throw an exception, raise CancelMutation.
 
     Keyword arguments are passed to the builtin `open` function.
-    Encoding defaults to UTF-8.
+    Uses :func:`open_with_detected_encoding` to detect & preserve the
+    encoding. The list of encodings can be overridden by supplying
+    `try_encodings`.
     """
-    simfile = open(filename, **kwargs)
+    simfile, encoding = open_with_detected_encoding(
+        filename,
+        try_encodings,
+        **kwargs
+    )
     try:
         yield simfile
     except CancelMutation:
@@ -98,5 +157,10 @@ def mutate(filename: str, **kwargs) -> Iterator[Simfile]:
     except:
         raise
     else:
-        with builtins.open(filename, 'w', **_open_args(kwargs)) as writer:
+        with builtins.open(
+            filename,
+            'w',
+            encoding=encoding,
+            **kwargs
+        ) as writer:
             simfile.serialize(writer)
--- simfile/tests/test_module.py
+++ simfile/tests/test_module.py
@@ -1,9 +1,19 @@
-from pyfakefs.fake_filesystem_unittest import TestCase
+# coding=utf-8
+from pyfakefs.fake_filesystem_unittest import TestCase # type: ignore
 
 import simfile
 from simfile.sm import SMSimfile
 from simfile.ssc import SSCSimfile
 from simfile.tests import test_sm, test_ssc
+
+test_encoding_strings = {
+    'utf-8': 'μουσικός',
+    'cp1252': 'música',
+    'cp932': 'ミュージシャン',
+    # It's surprisingly hard to find a CP949 string that's invalid for all the
+    # other encodings, which is why this string is nonsense:
+    'cp949': '뛃낥',
+}
 
 class TestSimfileModule(TestCase):
     def setUp(self):
@@ -12,6 +22,11 @@ class TestSimfileModule(TestCase):
             writer.write(test_sm.testing_simfile())
         with open('testing_simfile.ssc', 'w') as writer:
             writer.write(test_ssc.testing_simfile())
+        for encoding, artist in test_encoding_strings.items():
+            with open(f'{encoding}.sm', 'w', encoding=encoding) as writer:
+                writer.write(f'#TITLE:Song;\n#ARTIST:{artist};')
+        with open('invalid.sm', 'wb') as writer:
+            writer.write(b'#TITLE:Song;\n#ARTIST:\xc0\x00\x81;')
 
     def test_load_with_sm_extension(self):
         with open('testing_simfile.sm', 'r') as reader:
@@ -50,6 +65,49 @@ class TestSimfileModule(TestCase):
         
         self.assertIsInstance(ssc, SSCSimfile)
         self.assertEqual(SSCSimfile(string=test_ssc.testing_simfile()), ssc)
+    
+    def test_open_with_non_ascii_encodings(self):
+        for encoding, artist in test_encoding_strings.items():
+            with self.subTest(encoding):
+                sm = simfile.open(f'{encoding}.sm')
+                self.assertEqual(artist, sm.artist)
+    
+    def test_open_with_invalid_file(self):
+        self.assertRaises(UnicodeDecodeError, simfile.open, 'invalid.sm')
+    
+    def test_open_with_detected_encoding_with_non_ascii_encodings(self):
+        for encoding, artist in test_encoding_strings.items():
+            with self.subTest(encoding):
+                sm, enc = simfile.open_with_detected_encoding(f'{encoding}.sm')
+                self.assertEqual(encoding, enc)
+                self.assertEqual(artist, sm.artist)
+    
+    def test_open_with_detected_encoding_with_invalid_file(self):
+        self.assertRaises(
+            UnicodeDecodeError,
+            simfile.open_with_detected_encoding,
+            'invalid.sm',
+        )
+    
+    def test_open_with_detected_encoding_with_try_encodings(self):
+        sm, enc = simfile.open_with_detected_encoding(f'utf-8.sm', ['utf-8'])
+        self.assertEqual('utf-8', enc)
+        self.assertEqual(test_encoding_strings['utf-8'], sm.artist)
+
+        self.assertRaises(
+            UnicodeDecodeError,
+            simfile.open_with_detected_encoding,
+            'utf-8.sm',
+            ['cp949'],
+        )
+    
+    def test_open_with_detected_encoding_with_encoding_kwarg(self):
+        self.assertRaises(
+            TypeError,
+            simfile.open_with_detected_encoding,
+            'utf-8.sm',
+            encoding='cp949',
+        )
 
     def test_mutate_with_sm_file(self):
         with simfile.mutate('testing_simfile.sm') as sm:
