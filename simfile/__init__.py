9978c11d66ff130ddfa4f50c37192200dcfa3012
Ash Garcia <github@garcia.sh>
2021-06-01 18:08:22 -0700

    bump version

--- simfile/__init__.py
+++ simfile/__init__.py
@@ -11,7 +11,7 @@ from .types import Simfile
 from ._private.tee_file import tee_file
 
 
-__version__ = '2.0.0-beta.3'
+__version__ = '2.0.0-beta.4'
 __all__ = [
     'load', 'loads', 'open', 'open_with_detected_encoding', 'CancelMutation',
     'mutate',
