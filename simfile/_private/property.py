1c87b007e7d367815f4cad20452b2f1997986c55
Ash Garcia <github@garcia.sh>
2021-08-24 21:47:58 -0700

    remove unused decorator

--- simfile/_private/property.py
+++ simfile/_private/property.py
@@ -1,19 +1,6 @@
 from typing import Any, MutableMapping, Optional, cast
 
 
-def attr_property(private_name):
-
-    @property
-    def attr_property(self) -> str:
-        return cast(str, getattr(self, private_name))
-
-    @attr_property.setter
-    def attr_property(self, value: str) -> None:
-        setattr(self, private_name, value)
-
-    return attr_property
-
-
 def item_property(name):
 
     @property
