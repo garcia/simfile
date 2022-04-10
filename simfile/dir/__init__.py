fa412158643d5ebb5aa6b3bd3025907d740b6845
Ash Garcia <github@garcia.sh>
2022-04-10 13:20:59 -0700

    integrate w/ FS; add NativeOSFS compatibility layer

--- simfile/dir/__init__.py
+++ simfile/dir/__init__.py
@@ -1,11 +1,15 @@
 import os
 from typing import Iterator, List, Mapping, MutableMapping, NamedTuple, Optional, Sequence
 
-from . import fs
+from fs.base import FS
+import fs.path
+
+from ._private.nativeosfs import NativeOSFS
 from ..types import Simfile
+from .. import load as simfile_load
 
 
-__all__ = ['AssetFinder']
+__all__ = ['SimfileDirectory', 'SimfilePack']
 
 
 SIMFILE_EXTENSIONS = ['.sm', '.ssc']
@@ -59,7 +63,7 @@ ASSET_DEFINITIONS: Mapping[str, AssetDefinition] = {
 
 
 def _ext_property(ext: str):
-    @property
+    @property # type: ignore
     def ext_property(self: 'SimfileDirectory') -> Optional[str]:
         # `self.simfile` may be None because these properties are used to find
         # the simfile during AssetFinder initialization (if none was provided)
@@ -74,18 +78,20 @@ def _ext_property(ext: str):
     return ext_property
 
 def _asset_property(prop: str):
-    @property
+    @property # type: ignore
     def asset_property(self: 'SimfileDirectory') -> Optional[str]:
         if prop in self._cache: return self._cache[prop]
         
-        predefined_path = self.simfile.get(prop)
-        if predefined_path and self.filesystem.isfile(predefined_path):
-            return self._cache_path(prop, predefined_path)
+        specified_path = self.simfile.get(prop)
+        if specified_path:
+            full_path = self._join(self.simfile_dir, specified_path)
+            if self.filesystem.isfile(full_path):
+                return self._cache_path(prop, specified_path)
         
         asset_def = ASSET_DEFINITIONS[prop]
-        for asset_path in self._dirlist:
-            if asset_def.matches(asset_path):
-                return self._cache_path(prop, asset_path)
+        for file_in_simfile_dir in self._dirlist:
+            if asset_def.matches(file_in_simfile_dir):
+                return self._cache_path(prop, file_in_simfile_dir)
         
         return self._cache_path(prop, None)
 
@@ -105,39 +111,44 @@ class SimfileDirectory:
 
     simfile_dir: str
     simfile: Simfile
-    filesystem: fs.AbstractFileSystem
-
-    _dirlist: List[str]
-    _cache: MutableMapping[str, Optional[str]]
+    filesystem: FS
 
     def __init__(
         self,
         simfile_dir: str,
         *,
         simfile: Optional[Simfile] = None,
-        filesystem: fs.AbstractFileSystem = fs.OSFileSystem(),
+        filesystem: FS = NativeOSFS(),
     ):
         self.simfile_dir = simfile_dir
         self.filesystem = filesystem
-        self._dirlist = self.filesystem.listdir(simfile_dir)
-        self._cache: MutableMapping[str, str] = {}
+        self._dirlist: List[str] = self.filesystem.listdir(simfile_dir)
+        self._cache: MutableMapping[str, Optional[str]] = {}
 
         self.simfile = simfile or self._open_simfile()
     
     def _open_simfile(self) -> Simfile:
-        ssc = self.ssc
-        if ssc: return self.filesystem.open_simfile(ssc)
-
-        sm = self.sm
-        if sm: return self.filesystem.open_simfile(sm)
+        preferred_simfile = self.ssc or self.sm
+        if not preferred_simfile:
+            raise FileNotFoundError('no simfile in directory')
         
-        raise FileNotFoundError('no simfile in directory')
+        return simfile_load(self.filesystem.open(preferred_simfile))
+    
+    def _normpath(self, path: str) -> str:
+        if isinstance(self.filesystem, NativeOSFS):
+            return os.path.normpath(path)
+        else:
+            return fs.path.normpath(path)
+    
+    def _join(self, *paths: str) -> str:
+        if isinstance(self.filesystem, NativeOSFS):
+            return os.path.join(*paths)
+        else:
+            return fs.path.join(*paths)
     
     def _cache_path(self, key: str, value: Optional[str]) -> Optional[str]:
         if value is not None:
-            self._cache[key] = os.path.normpath(
-                os.path.join(self.simfile_dir, value)
-            )
+            self._cache[key] = self._normpath(self._join(self.simfile_dir, value))
         else:
             self._cache[key] = None
         return self._cache[key]
@@ -148,7 +159,7 @@ class SimfilePack:
         self,
         pack_dir: str,
         *,
-        filesystem: fs.AbstractFileSystem = fs.OSFileSystem(),
+        filesystem: FS = NativeOSFS(),
     ):
         self.pack_dir = pack_dir
         self.filesystem = filesystem
