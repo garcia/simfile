1c56d0e49fef2776a6a22ac099f194ca57b3f6e6
Ash Garcia <github@garcia.sh>
2024-12-07 14:41:08 -0800

    Fix readthedocs build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ import sphinx_rtd_theme
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-sys.path.insert(0, os.path.abspath("../../"))
+sys.path.insert(0, os.path.abspath("../../src"))
 
 import simfile
 
